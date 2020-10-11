5999107d39ef432f49b5e172c61582b36383c661
Artem10 <lyah.artem10@mail.ru>
2020-10-11 21:08:52 +0300

    preparing for heroku deployment

--- src/main/java/devanmejia/Application.java
+++ src/main/java/devanmejia/Application.java
@@ -3,13 +3,11 @@ package devanmejia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "devanmejia")
 @EnableJpaRepositories
 public class Application {
     @Bean
