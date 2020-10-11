b2a6d6901e6d3f65065d38dab66865299ff05280
Artem10 <lyah.artem10@mail.ru>
2020-10-11 23:16:39 +0300

    preparing for heroku deployment

--- src/main/java/devanmejia/app/Application.java
+++ src/main/java/devanmejia/app/Application.java
@@ -2,11 +2,13 @@ package devanmejia.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@ComponentScan("devanmejia")
 public class Application {
 
     public static void main(String[] args) {
