6ed33b1ac0158415871bcb1dc17ff0b5cacd2260
Artem10 <lyah.artem10@mail.ru>
2020-10-12 09:57:16 +0300

    preparing for heroku deployment

--- src/main/java/devanmejia/controllers/webFormsControllers/SignUpController.java
+++ src/main/java/devanmejia/controllers/webFormsControllers/SignUpController.java
@@ -3,7 +3,12 @@ package devanmejia.controllers.webFormsControllers;
 import devanmejia.models.entities.User;
 import devanmejia.services.SignUpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +17,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class SignUpController {
     @Autowired
     private SignUpService signUpService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @GetMapping("/signUp")
     public String getSignUpPage(Authentication authentication){
         if(authentication!=null)
@@ -21,6 +29,12 @@ public class SignUpController {
     @PostMapping("/signUp")
     public String signUpUser(User user){
         signUpService.signUpUser(user);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(user, user.getPassword());
+        Authentication auth = authenticationManager.authenticate(authenticationToken);
+        SecurityContext sc = SecurityContextHolder.getContext();
+        sc.setAuthentication(auth);
+
         return "redirect:/home";
     }
 }
