fcc9a91cfb2da62b668dcf0d0ab66655f4ab49a4
Artem10 <lyah.artem10@mail.ru>
2020-10-13 08:04:10 +0300

    preparing for heroku deployment

--- src/main/java/devanmejia/controllers/clientControllers/ShowUserOrdersController.java
+++ src/main/java/devanmejia/controllers/clientControllers/ShowUserOrdersController.java
@@ -1,6 +1,7 @@
 package devanmejia.controllers.clientControllers;
 
 import devanmejia.models.entities.Order;
+import devanmejia.models.entities.User;
 import devanmejia.repositories.OrderRepository;
 import devanmejia.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +12,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class ShowUserOrdersController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private OrderRepository orderRepository;
     @GetMapping("/showOrders")
     public @ResponseBody List<Order> showOrders(Authentication authentication){
-        if (userRepository.findById(authentication.getName()).isPresent()) {
-            System.out.println(userRepository.findById(authentication.getName()).get());
-            return userRepository.findById(authentication.getName()).get().getOrders();
-        }
+        Optional<User> userCandidate = userRepository.findById(authentication.getName());
+        if( userCandidate.isPresent())
+            return orderRepository.findAllByUser(userCandidate.get());
         return new ArrayList<>();
     }
 }
--- src/main/java/devanmejia/repositories/OrderRepository.java
+++ src/main/java/devanmejia/repositories/OrderRepository.java
@@ -4,10 +4,14 @@ package devanmejia.repositories;
 
 
 import devanmejia.models.entities.Order;
+import devanmejia.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findAllByUser(User user);
 
 }
