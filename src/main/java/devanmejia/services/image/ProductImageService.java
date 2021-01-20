b6b0d64fe8ab844d74703fde286fef689b057b0f
Artem10 <lyah.artem10@mail.ru>
2021-01-20 22:19:22 +0300

    add new resource file

--- src/main/java/devanmejia/services/image/ProductImageService.java
+++ src/main/java/devanmejia/services/image/ProductImageService.java
@@ -24,23 +24,10 @@ public class ProductImageService {
     @Value(value="classpath:static/product-images/")
     private Resource resource;
 
-    public void loadImageInDB(byte[] imageBytes, String productURL) throws IOException, URISyntaxException {
-        System.out.println(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "/static/product-images/" + productURL);
-        Path path = Paths.get(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "/static/product-images/" + productURL);
-        File newFile = path.toFile();
-        System.out.println(newFile.getAbsolutePath());
-        if (newFile.createNewFile()) {
-            try (BufferedOutputStream fileWriter = new BufferedOutputStream(new FileOutputStream(newFile));
-                 BufferedInputStream inputStream = new BufferedInputStream(new ByteArrayInputStream(Base64Decoder.decode(imageBytes, 0, imageBytes.length)))) {
-                IOUtils.copy(inputStream, fileWriter);
-            } catch (IOException e) {
-                e.printStackTrace();
-                throw new IllegalArgumentException(e.getMessage() + newFile.getAbsolutePath());
-            }
-        } else {
-            throw new IOException("File already exists" + newFile.getAbsolutePath());
-        }
-
+    public void loadImageInDB(byte[] imageBytes, String productURL) throws IOException {
+        String folder = "/product-images/";
+        Path path = Paths.get(folder + productURL);
+        Files.write(path, imageBytes);
     }
 
     public byte[] getImageFromDB(String productURL) throws IOException {
